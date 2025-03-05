66dfbf881acd19a48ee2dd7447d61b5a9e99b449
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2025-03-05 17:15:06 -0600

    18. Detalles adicionales sobre funciones

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -31,9 +31,20 @@
     // tarea asincrona para verificar nombre
     // ..
     // ..
-    if (fullName === "fernando") return false;
+    if (fullName === "emmanuel") return false;
 
     console.log("Crear actor");
     return true;
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    if (isDead) return 1500;
+    if (isSeparated) return 2500;
+
+    return isRetired ? 3000 : 4000;
+  };
 })();
