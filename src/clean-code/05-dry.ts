cec41904f88baec44ef067dc9f1504831b2e25f8
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2025-03-24 19:15:13 -0600

    22. Aplicando DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,17 +7,32 @@ class Product {
     public size: Size = ""
   ) {}
 
+  isProductReady(): boolean {
+    // Dry
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case "string":
+          if (this[key].length <= 0) throw Error(`${key} is empty`);
+          break;
+        case "number":
+          if (this[key] <= 0) throw Error(`${key} is zero`);
+          break;
+        default:
+          throw Error(`${typeof this[key]} is not valid`);
+      }
+    }
+
+    return true;
+  }
+
   toString() {
-    // No Dry
-    if (this.name.length <= 0) throw Error("Name is empty");
-    if (this.price <= 0) throw Error("Price is zero");
-    if (this.size.length <= 0) throw Error("Size is empty");
+    if (!this.isProductReady()) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
 }
 
 (() => {
-  const bluePants = new Product("Blue Large Pants", 10, "M");
+  const bluePants = new Product("Blue Large Pants", 10, "S");
   console.log(bluePants.toString());
 })();
